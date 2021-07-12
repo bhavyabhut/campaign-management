17eca9953aacf3c63e688237c98e73e9511057ef
Bhavya Bhut <pbhavy4@gmail.com>
2021-07-12 16:10:16 +0530

    redirect from dashboard

--- src/containers/Pages/Dashboard/index.jsx
+++ src/containers/Pages/Dashboard/index.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Card, Col, Divider, Row } from 'antd';
+import { useHistory } from 'react-router-dom';
 
 import style from './dashboard.module.css';
 
 const Dashboard = () => {
   const data = useSelector(state => state.Dashboard);
+  const history = useHistory();
+  const redirectToCampaign = () => history.push('/campaigns');
+
   return (
     <div className="site-card-wrapper">
       <Divider orientation="left">Campaign History</Divider>
       <Row gutter={16}>
-        <Col lg={6} md={12} sm={12} xs={24} className={style.mr}>
+        <Col onClick={redirectToCampaign} lg={6} md={12} sm={12} xs={24} className={style.mr}>
           <Card
             hoverable
             title="Total Campaigns"
